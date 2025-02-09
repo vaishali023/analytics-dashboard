518424550bd2f08105bd616c6f5bea87280f4f4e
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-02-08 22:26:35 -0500

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 className="text-4xl font-bold text-blue-600">
+      Hello world!
+    </h1>
+  </div>
+  )
+}
+
+export default App
