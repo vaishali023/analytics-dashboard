7311cafb0eff9467e0f5201209c571421747bf81
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-02-10 23:25:53 -0500

    Refactor: Update the fetch time

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -38,7 +38,7 @@ export const Dashboard: React.FC = () => {
     };
 
     fetchData();
-    const interval = setInterval(fetchData, 5000);
+    const interval = setInterval(fetchData, 3000);
     return () => clearInterval(interval);
   }, []);
 
